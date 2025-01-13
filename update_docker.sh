71ab6531ed6d8e5f8d69975ee077cc436fcd30ee
root <root@ip-10-1-0-185.us-east-2.compute.internal>
2025-01-13 17:56:56 +0000

    commit webhook

--- update_docker.sh
+++ update_docker.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+# Navegar para o diretório do repositório
 cd /root/RepoTeste/RepoTeste
+
+# Puxar as últimas alterações do repositório
 git pull origin main
+
+# Construir a imagem Docker
 docker build -t myapp .
-docker stop $(docker ps -q --filter ancestor=myapp)
+
+# Parar e remover qualquer container que esteja usando a porta 3000
+docker ps -q --filter "publish=3000" | xargs -r docker stop
+docker ps -q --filter "publish=3000" | xargs -r docker rm
+
+# Iniciar um novo container na porta 3000
 docker run -d -p 3000:3000 myapp
+
